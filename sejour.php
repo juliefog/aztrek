6fb247d2a69a5cc1617adfb32356bc1b795c6358
juliefog <j.foglietti@gmail.com>
2019-01-22 09:21:08 +0100

    modification de la page séjour : get footer./

--- sejour.php
+++ sejour.php
@@ -2,16 +2,12 @@
 require_once 'model/database.php';
 require_once 'functions.php';
 
-
-
-
-
 getMenu();
 getHeader("pays", "la page des séjours du pays");
 
 ?>
 
-        <!-- MAIN - slide sejour -->
+        <!-- MAIN -->
         <div class="sejour-top">
             <h1 class="itineraire">Caminando Mexico</h1>
             <p>Bienvenue aux randonneurs pour une découverte énergisante du Mexique ! L’altiplano et les légendes de
@@ -151,49 +147,4 @@ getHeader("pays", "la page des séjours du pays");
         <!-- SECTION RESERVATION FIN -->
 
 
-        <!-- FOOTER -->
-        <footer>
-            <div class="item-footer">
-                <div class="item1-footer">
-                    <div class="logo-footer">
-                        <a href="index.php"><img src="images/logo_jaune_petit.svg" alt="Aztrek"></a>
-                    </div>
-                    <a href="#">Qui sommes-nous ?</a>
-                </div>
-
-                <div class="item2-footer">
-                    <div class="newsletter">
-                        <p>Newsletter</p>
-                        <input type="email" name="email" id="email" placeholder="votre email">
-                        <button class="btn" type="submit">S'abonner</button>
-                    </div>
-                    <div class="reseau-footer">
-                        <img src="images/instagram-footer.png" alt="instagram aztrek">
-                        <img src="images/facebook-footer.png" alt="facebook aztrek">
-                        <img src="images/twitter-footer.png" alt="twitter aztrek">
-                        <img src="images/youtube-footer.png" alt="youtube aztrek">
-                    </div>
-                </div>
-
-                <div class="item3-footer">
-                    <a href="#">Mention légales</a>
-                    <p>Nos destinations</p>
-                    <a href="pays.php">Mexique</a>
-                    <a href="#">Guatemala</a>
-                    <a href="#">Salvador</a>
-                    <a href="#">Honduras</a>
-                    <a href="#">Costa Rica</a>
-                </div>
-            </div>
-        </footer>
-    </div>
-
-
-</body>
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/jquery.sidr.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/scripts.js"></script>
-
-</html>
+       <?php getFooter() ?>
